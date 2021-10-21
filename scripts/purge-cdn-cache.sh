fd5aaf7e86eda24a067afd264c1e982faf593a40
pscanf <paolo.scanferla@gmail.com>
2021-10-21 09:09:56 +0300

    chore: add cache-purging to deploy CI workflow

---
+++ scripts/purge-cdn-cache.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+curl -X POST "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE_ID/purge_cache" \
+     -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
+     -H "Content-Type: application/json" \
+     --data '{"purge_everything":true}'
