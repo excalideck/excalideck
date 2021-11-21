2e00aa9379cc42e1fad9add65454a7c57ce1e36e
pscanf <paolo.scanferla@gmail.com>
2021-11-21 19:51:13 +0200

    fix: fix cropping slide canvases in webkit browsers

--- packages/slide-renderers/src/renderers/canvasSlideRenderer/README.md
+++ packages/slide-renderers/src/renderers/canvasSlideRenderer/README.md
@@ -17,9 +17,9 @@ printable area.
 ### Difficulty
 
 The main difficulty with this approach is identifying **where** the printable
-area is located inside the canvas. This is because the `exportToCanvas` function
-produces a canvas with a coordinate system where `(0, 0)` is assigned to the
-top-left-most point of the set of rendered Excalidraw elements.
+area is located inside the canvas. This is because Excalidraw's `exportToCanvas`
+function produces a canvas with a coordinate system where `(0, 0)` is assigned
+to the top-left-most point of the set of rendered Excalidraw elements.
 
 ![](./coordinate-systems.png)
 
@@ -32,9 +32,23 @@ to those same coordinates wouldn't produce the desired effect.
 ### Solution
 
 The chosen way to overcome this difficulty is to add a "far away" Excalidraw
-element, a small rectangle whose top-left-most point is _most likely_ above and
-more-to-the-left any other element. Knowing the Excalidraw-coordinates of this
-rectangle, and knowing that its top-left-most point will _most likely_ have
-canvas-coordinates `(0,0)`, we can then figure out the correct cropping area.
+element, a rectangle whose top-left-most point is _most likely_ above and
+more-to-the-left-than any other element. Knowing the Excalidraw-coordinates of
+the top-left-most point of this rectangle, and knowing that said point will
+_most likely_ have canvas-coordinates `(0,0)`, we can then figure out the
+correct cropping area.
 
 ![](./correct-cropping.png)
+
+### Notes
+
+- when calling `CanvasRenderingContext2D.drawImage` in webkit browsers, if we
+  pass cropping coordinates that exceed the bounds of the source canvas (in our
+  case the canvas returned by Excalidraw's `exportToCanvas`), we get unexpected
+  results ([read some details](https://stackoverflow.com/q/35500999)). To avoid
+  this, we set height and width of the "far away" rectangle so that it
+  encompasses the whole printable area, hence guaranteeing that the cropping
+  coordinates won't exceed the bounds of canvas returned by Excalidraw's
+  `exportToCanvas`
+- Excalidraw's `exportToCanvas` function adds some padding around the scene, so
+  that needs to be taken into account when cropping
--- packages/slide-renderers/src/renderers/canvasSlideRenderer/index.ts
+++ packages/slide-renderers/src/renderers/canvasSlideRenderer/index.ts
@@ -1,33 +1,10 @@
-import { Deck, DeckOperations, Hash } from "@excalideck/deck";
+import { Deck, DeckOperations, Hash, PrintableArea } from "@excalideck/deck";
 import { exportToCanvas } from "@excalidraw/excalidraw";
 import { nanoid } from "nanoid";
 import SlideRenderer from "../../SlideRenderer";
 import lruMemoize from "../../utils/lruMemoize";
 
 const EXCALIDRAW_EXPORT_DEFAULT_PADDING = 10;
-const FAR_AWAY_RECTANGLE = Object.freeze({
-    id: "FAR_AWAY_RECTANGLE",
-    versionNonce: 0,
-    version: 0,
-    type: "rectangle",
-    x: -5_000,
-    y: -5_000,
-    width: 1,
-    height: 1,
-    angle: 0,
-    strokeColor: "transparent",
-    backgroundColor: "transparent",
-    fillStyle: "hachure",
-    strokeWidth: 1,
-    strokeStyle: "solid",
-    roughness: 0,
-    opacity: 100,
-    groupIds: [],
-    strokeSharpness: "sharp",
-    seed: 0,
-    isDeleted: false,
-    boundElementIds: null,
-});
 
 const canvasSlideRenderer: SlideRenderer<HTMLCanvasElement> = {
     renderSlide: lruMemoize(
@@ -35,12 +12,13 @@ const canvasSlideRenderer: SlideRenderer<HTMLCanvasElement> = {
             const slide = DeckOperations.getSlide(deck, slideId);
             const { printableArea } = deck;
 
+            const farAwayRectangle = getFarAwayRectangle(printableArea);
             const excalidrawElements = [
                 ...slide.excalidrawElements,
                 ...(slide.shouldRenderWithCommonExcalidrawElements
                     ? deck.commonExcalidrawElements
                     : []),
-                FAR_AWAY_RECTANGLE,
+                farAwayRectangle,
             ];
 
             const drawingBoardCanvas = exportToCanvas({
@@ -49,8 +27,8 @@ const canvasSlideRenderer: SlideRenderer<HTMLCanvasElement> = {
 
             const slideCanvas = cropCanvas(
                 drawingBoardCanvas,
-                EXCALIDRAW_EXPORT_DEFAULT_PADDING - FAR_AWAY_RECTANGLE.x,
-                EXCALIDRAW_EXPORT_DEFAULT_PADDING - FAR_AWAY_RECTANGLE.y,
+                EXCALIDRAW_EXPORT_DEFAULT_PADDING - farAwayRectangle.x,
+                EXCALIDRAW_EXPORT_DEFAULT_PADDING - farAwayRectangle.y,
                 printableArea.width,
                 printableArea.height
             );
@@ -84,4 +62,30 @@ function cropCanvas(
         .getContext("2d")!
         .drawImage(canvas, left, top, width, height, 0, 0, width, height);
     return croppedCanvas;
+}
+
+function getFarAwayRectangle(printableArea: PrintableArea) {
+    return {
+        id: "FAR_AWAY_RECTANGLE",
+        versionNonce: 0,
+        version: 0,
+        type: "rectangle",
+        x: -5_000,
+        y: -5_000,
+        width: 5_000 + printableArea.width + 10,
+        height: 5_000 + printableArea.height + 10,
+        angle: 0,
+        strokeColor: "transparent",
+        backgroundColor: "transparent",
+        fillStyle: "hachure",
+        strokeWidth: 1,
+        strokeStyle: "solid",
+        roughness: 0,
+        opacity: 100,
+        groupIds: [],
+        strokeSharpness: "sharp",
+        seed: 0,
+        isDeleted: false,
+        boundElementIds: null,
+    };
 }
